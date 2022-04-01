368d14e9548efbf470fc764502a5dd1988f56990
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-04-01 10:35:29 -0700

    Back button
    Back button added to child activity ThreeActivity

--- app/src/main/java/com/cetis108/papasconchorizo/ThreeActivity.kt
+++ app/src/main/java/com/cetis108/papasconchorizo/ThreeActivity.kt
@@ -7,5 +7,6 @@ class ThreeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_three)
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 }
