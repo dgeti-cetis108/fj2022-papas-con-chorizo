afbe5a2aa4110c8b5e5b2b9faf8834e23a3dfc08
Bidkar Aragón Cárdenas <bidkar@gmail.com>
2022-04-01 09:31:15 -0700

    Navegar entre activities
    Practica: agregar 3 activities, ir de la uno a las dos sin regreso, ir de la dos a las tres con regreso
    TODO: agregar boton back al three

--- app/src/main/java/com/cetis108/papasconchorizo/TwoActivity.kt
+++ app/src/main/java/com/cetis108/papasconchorizo/TwoActivity.kt
@@ -1,11 +1,19 @@
 package com.cetis108.papasconchorizo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class TwoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_two)
+
+        val  btnGoThree = findViewById<Button>(R.id.buttonTwoGoThree)
+        btnGoThree.setOnClickListener {
+            val intent = Intent(this, ThreeActivity::class.java)
+            this.startActivity(intent)
+        }
     }
 }
